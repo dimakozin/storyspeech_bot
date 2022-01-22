3ad6d9f985e07bcf82fd8c8f058f2eaa218e7988
dimakozin <dimakozin@gmail.com>
2022-01-22 17:06:51 +0000

    Parse articles. Partical messages (4096 symbols)

--- Scenarios/functions.mjs
+++ Scenarios/functions.mjs
@@ -5,19 +5,28 @@ import axios from 'axios'
 import xpath from 'xpath'
 import { DOMParser } from 'xmldom'
 
+const MESSAGE_LENGTH = 4096
+
 export default {
-    RepeatMessage: (bot, chatId, messageText) => {
+    ParseSite: (bot, chatId, messageText) => {
         // TODO: xpath to parse TJ
-        // TODO: tts parsed text 
+        // TODO: tts parsed text
 
         axios.get(messageText)
         .then( response => {
             bot.sendMessage(chatId, 'Бот начал надиктовывать текст ☺ \n⌛ Нужно немножко подождать ')
             let page = response.data
-
             let text = parseData(page)
-            bot.sendMessage(chatId, text)
 
+            let messages = []
+
+            if(text.length > MESSAGE_LENGTH){
+                messages = text.match(/(.|[\r\n]){1,4096}/g)
+            } else messages = [text]
+
+            messages.forEach( (message) => {
+                bot.sendMessage(chatId, message)
+            })
         })
         .catch (error => {
             bot.sendMessage(chatId, 'Произошла ошибка')
@@ -27,13 +36,29 @@ export default {
 }
 
 const TJ_XPath = {
-    title: '//h1'
+    title: '//h1',
+    mainText: "//div[@class='l-entry__content']//div[@class='l-island-a']"
 }
 
 let parseData = (data) => {
     let doc = new DOMParser().parseFromString(data)
     let titleNodes = xpath.select(TJ_XPath.title, doc)
-    let title = titleNodes[0].childNodes[0].nodeValue
+    let title = titleNodes[0].childNodes[0].toString()
 
-    return title
+    let mainTextNodes = xpath.select(TJ_XPath.mainText, doc) 
+    let mainText = mainTextNodes.map( (item) => removeTags(item.toString()
+    .replaceAll("\n", '')
+    .replaceAll('   ', '')))
+
+    mainText.splice(0,0,title)
+    return mainText.join(' ')
+}
+
+let removeTags = (str) => {
+    if ((str===null) || (str===''))
+        return false;
+    else
+        str = str.toString();
+          
+    return str.replace( /(<([^>]+)>)/ig, '');
 }
